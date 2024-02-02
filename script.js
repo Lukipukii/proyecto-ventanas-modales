83be98c2cf80b20597ad18e370d0f4d21cb43e4c
Lucas Colban <lukipuki.git@gmail.com>
2024-02-02 12:19:07 +0100

    Configurada tecla ESC para salir de las ventanas

--- script.js
+++ script.js
@@ -14,9 +14,21 @@ const closeModal = () => {
   overlay.classList.add('hidden')
 }
 
-//TODO: Configurar listeners
+//listeners
+
+//configurar a cada boton que al clicar sobre el se abra la ventana
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal)
+
+//al pulsar fuera de la venta o en la "X" se cierra la misma
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
-//
+
+//Añadimos un evento global, que en para poder cerrar las ventanas si se presiona la tecla ESC
+document.addEventListener('keydown', function (e) {
+  //Solo se ejecutará la funcion closeModal si presionamos la tecla ESC
+  //Y tambien si el div que tiene la clase modal no tiene la clase hidden
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
