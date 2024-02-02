b97f5f39e19e8f060e3b6cfbe954f76f883b8db6
Lucas Colban <lukipuki.git@gmail.com>
2024-02-02 12:06:00 +0100

    Proyecto iniciado y selección de elementos DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+//Selección de las clases del documento HTML
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+//TODO: funciones para para cambiar clase hidden
+//TODO: Configurar listeners
+//
